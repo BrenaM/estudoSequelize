12f4ab4b0f48c67bd2793bd04bc687498ea77f73
BrenaM <brenamonteiro_@hotmail.com>
2021-04-12 10:45:17 -0300

    usando operadore Sequelize

--- instagram.js
+++ instagram.js
@@ -1,37 +1,78 @@
 const { Usuario, Post, Comentario, sequelize } = require('./models');
+const {Op} = require('sequelize');
 
-
-Usuario.findAll()
-.then ((resultado) => {
-    console.table(resultado.map(user => user.toJSON()));
-    //console.log(resultado.map(user => user.toJSON()));
-   
-});
-
-Post.findAll()
-.then ((resultado) => {
-    console.table(resultado.map(user => user.toJSON()));
-    //console.log(resultado.map(user => user.toJSON()));
-   
-});
-
-Comentario.findAll()
-.then ((resultado) => {
-    console.table(resultado.map(user => user.toJSON()));
-    //console.log(resultado.map(user => user.toJSON()));
-   
-});
-
-// const { Usuario, Post, sequelize } = require('./models');
 
 // Usuario.findAll()
-// .then((resultado) => {
-//     console.log(resultado.map(user => user.toJSON()));
+// .then ((resultado) => {
+//     console.table(resultado.map(user => user.toJSON()));
+//     //console.log(resultado.map(user => user.toJSON()));
+   
 // });
-
 
 // Post.findAll()
-// .then((resultado) => {
-//     console.log(resultado.map(user => user.toJSON()));
+// .then ((resultado) => {
+//     console.table(resultado.map(user => user.toJSON()));
+//     //console.log(resultado.map(user => user.toJSON()));
+   
 // });
+
+// Comentario.findAll()
+// .then ((resultado) => {
+//     console.table(resultado.map(user => user.toJSON()));
+//     //console.log(resultado.map(user => user.toJSON()));
+   
+// });
+
+// Post.findAll({
+//     where: {
+//         //usuarios_id: 1
+//         texto: {[Op.like]: 'oi%'}
+//     }
+// })
+// .then ((resultado) => {
+//     console.table(resultado.map(user => user.toJSON())); 
+   
+// });
+
+// Usuario.findAll({
+//     order:[
+//         ['id', 'ASC']  //ASC=crescente DESC= decrescente
+//     ],
+//     limit: 2,  // de todos os resultados eu sóquero 2
+//     offset: 2   //vai pular os dois primeiros e trazer 3 e 4
+// })
+// .then ((resultado) => {
+//     console.table(resultado.map(user => user.toJSON()));
+//     //console.log(resultado.map(user => user.toJSON()));
+   
+// });
+
+Usuario.findAll({
+    where:{
+        nome: {[Op.like]: '%a%'}
+    }
+})
+.then ((resultado) => {
+    console.table(resultado.map(user => user.toJSON())); 
+});
+
+Usuario.findAll({
+    where:{
+        nome: {[Op.notLike]: '%a%'}
+    }
+})
+.then ((resultado) => {
+    console.table(resultado.map(user => user.toJSON())); 
+});
+
+Comentario.findAll({
+    order:[
+        ['id', 'ASC']  
+    ],
+    limit: 2, 
+    offset: 2
+})
+.then ((resultado) => {
+    console.table(resultado.map(user => user.toJSON())); 
+});
 
--- models/Post.js
+++ models/Post.js
@@ -15,21 +15,3 @@ module.exports = (sequelize, DataTypes) => {
     return Post;
 }
 
-// module.exports = (sequelize, DataTypes) => {
-    
-//     const Post = sequelize.define(
-//         'Post', {
-//             texto: DataTypes.STRING(100),
-//             img: DataTypes.STRING(100),
-//             usuarios_id: DataTypes.INTEGER,
-//             n_likes: DataTypes.INTEGER
-//         }, 
-//         {
-//             tableName: "posts",
-//             timestamps: false
-//         }
-//     );
-
-//     return Post;
-
-// }
